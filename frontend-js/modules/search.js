9952186fb32c59da88cfb60856cb5dafb8ecceb3
Afiq Syazwan <fafiqsyaz@gmail.com>
2020-08-02 00:29:36 +0800

    add search features v1

---
+++ frontend-js/modules/search.js
@@ -0,0 +1,114 @@
+import axios from "axios"
+
+export default class Search {
+	constructor() {
+		this.injectHTML()
+		this.textField = document.querySelector(".live-search-field")
+		this.overlay = document.querySelector(".search-overlay")
+		this.closeIcon = document.querySelector(".close-live-search")
+		this.headerSearch = document.querySelector(".header-search-icon")
+		this.resultSearch = document.querySelector(".live-search-results")
+		this.loaderIcon = document.querySelector(".circle-loader")
+		this.typingWaitTimer
+		this.previousValue = ""
+		
+		this.event()
+	}
+
+	// live-search-results--visible
+
+	// Events
+	event() {
+		this.textField.addEventListener("keyup", (e) => this.keyPressHandler())
+		this.closeIcon.addEventListener("click", () => this.closeSearch())
+		this.headerSearch.addEventListener("click", (e)=>{
+			e.preventDefault()
+			this.openOverlay()
+		})
+	}
+
+
+	// Methods
+	openOverlay() {
+		this.overlay.classList.add("search-overlay--visible")
+		setTimeout(()=>{
+			this.textField.focus()
+		}, 50)
+	}
+
+	keyPressHandler(){
+		let value = this.textField.value
+
+		if(value != "" && value != this.previousValue){
+			clearTimeout(this.typingWaitTimer)
+			this.showLoaderIcon()
+			this.typingWaitTimer = setTimeout(() => {
+				this.sendRequest()
+			}, 3000)
+		}
+		console.log(this.typingWaitTimer)
+
+		this.previousValue = value
+		// 
+	}
+
+	sendRequest(){
+		axios.post("/search", {setValue: this.textField.value})
+		.then(() => {
+
+		})
+		.catch(() => {
+			alert("Hello, the request has failed")
+		})
+		
+	}
+
+	showLoaderIcon(){
+		this.loaderIcon.classList.add("circle-loader--visible")
+	}
+
+
+	closeSearch(){
+		this.overlay.classList.remove("search-overlay--visible")
+	}
+
+	injectHTML(){
+		document.body.insertAdjacentHTML("beforeend", `<div class="search-overlay">
+    <div class="search-overlay-top shadow-sm">
+      <div class="container container--narrow">
+        <label for="live-search-field" class="search-overlay-icon"><i class="fas fa-search"></i></label>
+        <input type="text" id="live-search-field" class="live-search-field" placeholder="What are you interested in?">
+        <span class="close-live-search"><i class="fas fa-times-circle"></i></span>
+      </div>
+    </div>
+
+    <div class="search-overlay-bottom">
+      <div class="container container--narrow py-3">
+        <div class="circle-loader"></div>
+        <div class="live-search-results">
+          <div class="list-group shadow-sm">
+            <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
+
+            <a href="#" class="list-group-item list-group-item-action">
+              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #1</strong>
+              <span class="text-muted small">by barksalot on 0/14/2019</span>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action">
+              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #2</strong>
+              <span class="text-muted small">by brad on 0/12/2019</span>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action">
+              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #3</strong>
+              <span class="text-muted small">by barksalot on 0/14/2019</span>
+            </a>
+            <a href="#" class="list-group-item list-group-item-action">
+              <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #4</strong>
+              <span class="text-muted small">by brad on 0/12/2019</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`)
+	}
+}
